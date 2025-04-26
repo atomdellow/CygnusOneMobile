287e4f34edc0e46f3e20b1ff9cd2ad40a9cf2486
kendron2357 <atomdellow@gmail.com>
2025-04-26 14:01:19 -0400

    initial commit of .net's Maui for Android and Windows.

---
+++ CygnusOneMobile/CygnusOneMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CygnusOneMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CygnusOneMobile/CygnusOneMobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CygnusOneMobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
